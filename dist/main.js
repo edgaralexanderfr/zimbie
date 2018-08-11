edb0ec86452eec4c1bd3549f46e6c776ac118ad3
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2018-08-11 18:42:45 -0500

    Remove comments when compiling TypeScript.

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,4 @@
 "use strict";
-/// <reference path="../node_modules/phaser-ce/typescript/phaser.d.ts" />
 var GAME_WIDTH = 640;
 var GAME_HEIGHT = 360;
 var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, "", {
