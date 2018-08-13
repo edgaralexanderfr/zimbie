27d3c51218a4d953f4c050b5db3e6d2174df78d9
Edgar Alexander Franco <edgar.franco@alegra.com>
2018-08-13 13:33:35 -0500

    Define getProjectCurrentVersion(function) in gulpfile.js to retrieve the current version for the project and 'git-describe' is also added as a devDependency.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var { gitDescribe } = require('git-describe');
+
+function getProjectCurrentVersion(callback) {
+  gitDescribe(__dirname, { customArguments: ['--abbrev=40'] }, (err, gitInfo) => {
+    if (err) {
+      throw err;
+    }
+
+    if (gitInfo.tag) {
+      callback(gitInfo.tag);
+    } else {
+      callback(gitInfo.hash);
+    }
+  });
+}
 
 gulp.task('uglify', function () {
   return gulp.src('dist/main.js')
