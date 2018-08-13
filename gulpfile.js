5e8139760754d00a9e84160e6448e1a7eaabc145
Edgar Alexander Franco <edgar.franco@alegra.com>
2018-08-13 14:53:53 -0500

    Define documented function util/damage(number, number) as prototype and add gulp task for generating typedoc documentation with its dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var { gitDescribe } = require('git-describe');
+var typedoc = require('gulp-typedoc');
 
 function getProjectCurrentVersion(callback) {
   gitDescribe(__dirname, { customArguments: ['--abbrev=40'] }, (err, gitInfo) => {
@@ -20,4 +21,18 @@ gulp.task('uglify', function () {
   return gulp.src('dist/main.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/min'));
+});
+
+gulp.task('typedoc', function (done) {
+  getProjectCurrentVersion(function (version) {
+    gulp.src(['src/'])
+      .pipe(typedoc({
+        module: 'amd',
+        target: 'es5',
+        out: './docs/' + version,
+        name: 'Zimbie ' + version
+      }));
+
+    done();
+  });
 });
