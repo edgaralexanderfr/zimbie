60066dcb7a65c12ea6d21c3ea90c7646bf7f7496
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2018-12-24 19:36:00 -0500

    Export animations sprites through 'spritify.py'.

--- res/models/blender/scripts/spritify.py
+++ res/models/blender/scripts/spritify.py
@@ -13,6 +13,7 @@ angles_names = ['down', 'down-right', 'right', 'up-right', 'up', 'up-left', 'lef
 angles_values = [0, 45, 90, 135, 180, 225, 270, 315]
 cls_on_run = True
 layer_count = 3
+ignored_actions = []
 ################################################################################################
 
 # Save current scene state:
@@ -43,61 +44,80 @@ bpy.data.scenes['Scene'].render.resolution_x = resolution_x
 bpy.data.scenes['Scene'].render.resolution_y = resolution_y
 
 try :
+  # FIXME: rmtree is always throwing a FileNotFoundError exception...
   shutil.rmtree(path)
 except :
   pass
 
-# Hide every layer:
-for l in range(0, layer_count) :
-  bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[l] = False
+initial_action = armature.animation_data.action
+action_count = len(bpy.data.actions)
 
-# Iterate over each mesh from each layer:
-for l in range(0, layer_count) :
-  bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[l] = True
+# Iterate over each animation:
+for a in range(0, action_count) :
+  # We check if it's an ignored action, if so we jump to the next one:
+  if (bpy.data.actions[a].name in ignored_actions) :
+    continue
 
-  # We hide the previous layer after showing the current one, if we're at
-  # the first layer then we proceed to hide the last one:
-  if (l == 0) :
-    bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[layer_count - 1] = False
-  else :
-    bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[l - 1] = False
+  armature.animation_data.action = bpy.data.actions[a]
 
-  # Hide every object within current layer:
-  for ob in bpy.data.objects :
-    if (ob.type == 'MESH' and ob.layers[l]) :
-      ob.hide_render = True
+  # Hide every layer:
+  for l in range(0, layer_count) :
+    bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[l] = False
 
-  # Iterate over each object within current layer independently:
-  for ob in bpy.data.objects :
-    if (ob.type == 'MESH' and ob.layers[l]) :
-      ob.hide_render = False
+  # Iterate over each mesh from each layer:
+  for l in range(0, layer_count) :
+    bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[l] = True
 
-      for i, angle in enumerate(angles_values) :
-        armature.rotation_euler = (0, 0, (angle * math.pi) / 180)
-        bpy.data.scenes['Scene'].render.filepath = path + 'character/' + ob.name.lower() + '/' + armature.animation_data.action.name.lower() + '/' + angles_names[i] + '/'
+    # We hide the previous layer after showing the current one, if we're at
+    # the first layer then we proceed to hide the last one:
+    if (l == 0) :
+      bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[layer_count - 1] = False
+    else :
+      bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[l - 1] = False
 
-        try :
-          os.makedirs(bpy.data.scenes['Scene'].render.filepath)
-        except OSError :
-          pass
+    # Hide every object within current layer:
+    for ob in bpy.data.objects :
+      if (ob.type == 'MESH' and ob.layers[l]) :
+        ob.hide_render = True
 
-        print(bpy.data.scenes['Scene'].render.filepath)
-        bpy.ops.render.render(animation=True)
+    # Iterate over each object within current layer independently:
+    for ob in bpy.data.objects :
+      if (ob.type == 'MESH' and ob.layers[l]) :
+        ob.hide_render = False
 
-      ob.hide_render = True
+        for i, angle in enumerate(angles_values) :
+          armature.rotation_euler = (0, 0, (angle * math.pi) / 180)
+          bpy.data.scenes['Scene'].render.filepath = path + 'character/' + ob.name.lower() + '/' + armature.animation_data.action.name.lower() + '/' + angles_names[i] + '/'
 
-  # Show every object within current layer again:
-  for ob in bpy.data.objects :
-    if (ob.type == 'MESH' and ob.layers[l]) :
-      ob.hide_render = False
+          try :
+            os.makedirs(bpy.data.scenes['Scene'].render.filepath)
+          except OSError :
+            pass
 
-# Show every layer again:
-for l in range(0, layer_count) :
-  bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[l] = True
+          print(bpy.data.scenes['Scene'].render.filepath)
+          bpy.ops.render.render(animation=True)
+
+        ob.hide_render = True
+
+    # Show every object within current layer again:
+    for ob in bpy.data.objects :
+      if (ob.type == 'MESH' and ob.layers[l]) :
+        ob.hide_render = False
+
+  # Show every layer again:
+  for l in range(0, layer_count) :
+    bpy.data.scenes['Scene'].render.layers['RenderLayer'].layers[l] = True
+
+try :
+  # FIXME: rmtree is always throwing a FileNotFoundError exception...
+  shutil.rmtree(path)
+except :
+  pass
 
 print('All sprites exported correctly.')
 
 # Reassign initial values:
+armature.animation_data.action = initial_action
 armature.rotation_euler.z = initial_armature_rotation_z
 bpy.data.scenes['Scene'].render.filepath = initial_filepath
 bpy.data.scenes['Scene'].frame_step = initial_frame_step
