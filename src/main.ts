924c29e38082ff7971f3069d3e98d6f09111bda1
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2019-01-05 18:18:49 -0500

    Initial creation of the Character class and update 'spritify.py' description.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@
 
 import AssetManager from "./AssetManager";
 import Terrain from "./Terrain";
+import Character from "./Character";
+import { Key } from "phaser-ce";
 
 const GAME_WIDTH: number = 1350;
 const GAME_HEIGHT: number = 650;
-const CAMERA_SPEED: number = 8;
+const CAMERA_SPEED: number = 4;
 
 let game: Phaser.Game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, "", {
   preload: preload,
@@ -17,6 +19,14 @@ let upKey: Phaser.Key;
 let downKey: Phaser.Key;
 let leftKey: Phaser.Key;
 let rightKey: Phaser.Key;
+let zeroKey: Phaser.Key;
+let oneKey: Phaser.Key;
+let twoKey: Phaser.Key;
+let threeKey: Phaser.Key;
+let fourKey: Phaser.Key;
+let fiveKey: Phaser.Key;
+
+let character: Character;
 
 function preload(): void {
   document.body.style.backgroundColor = "black";
@@ -32,27 +42,117 @@ function create(): void {
   downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
   leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
   rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+  zeroKey = game.input.keyboard.addKey(Phaser.Keyboard.ZERO);
+  oneKey = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+  twoKey = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
+  threeKey = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
+  fourKey = game.input.keyboard.addKey(Phaser.Keyboard.FOUR);
+  fiveKey = game.input.keyboard.addKey(Phaser.Keyboard.FIVE);
 }
 
 function init(): void {
   let terrain: Terrain = new Terrain(game);
   terrain.generate();
+
+  character = new Character(game, 675, 325);
+  character.setClothing("hair", 1);
+  character.setClothing("jacket", 1);
+  character.setClothing("shirt", 1);
+  character.setClothing("pants", 1);
+  character.setClothing("shoes", 1);
+  game.camera.follow(character.body, Phaser.Camera.FOLLOW_LOCKON);
 }
 
 function update(): void {
   if (upKey.isDown) {
-    game.camera.y -= CAMERA_SPEED;
+    character.body.y -= CAMERA_SPEED;
   }
 
   if (downKey.isDown) {
-    game.camera.y += CAMERA_SPEED;
+    character.body.y += CAMERA_SPEED;
   }
 
   if (leftKey.isDown) {
-    game.camera.x -= CAMERA_SPEED;
+    character.body.x -= CAMERA_SPEED;
   }
 
   if (rightKey.isDown) {
-    game.camera.x += CAMERA_SPEED;
+    character.body.x += CAMERA_SPEED;
+  }
+
+  if (zeroKey.isDown) {
+    let clothingName: any = prompt("Clothing name:");
+    let clothingColor: any = prompt("Clothing colour:");
+    character.setClothingColor(clothingName, +clothingColor);
+  }
+
+  if (oneKey.isDown) {
+    character.nextClothing("hair");
+  }
+
+  if (twoKey.isDown) {
+    character.nextClothing("jacket");
+  }
+
+  if (threeKey.isDown) {
+    character.nextClothing("shirt");
+  }
+
+  if (fourKey.isDown) {
+    character.nextClothing("pants");
+  }
+
+  if (fiveKey.isDown) {
+    character.nextClothing("shoes");
+  }
+
+  // Update character animations:
+
+  if (upKey.isDown && leftKey.isDown) {
+    character.playAnimation("walkUpLeft");
+
+    return;
+  }
+
+  if (upKey.isDown && rightKey.isDown) {
+    character.playAnimation("walkUpRight");
+
+    return;
+  }
+
+  if (downKey.isDown && leftKey.isDown) {
+    character.playAnimation("walkDownLeft");
+
+    return;
+  }
+
+  if (downKey.isDown && rightKey.isDown) {
+    character.playAnimation("walkDownRight");
+
+    return;
+  }
+
+  if (upKey.isDown) {
+    character.playAnimation("walkUp");
+
+    return;
+  }
+
+  if (downKey.isDown) {
+    character.playAnimation("walkDown");
+
+    return;
+  }
+
+  if (leftKey.isDown) {
+    character.playAnimation("walkLeft");
+
+    return;
+  }
+
+  if (rightKey.isDown) {
+    character.playAnimation("walkRight");
+
+    return;
   }
 }
